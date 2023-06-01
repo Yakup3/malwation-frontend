b0d14976aee3a1ea9d17f187d64cb28a34706359
Yakup Malikov <yakup@Yakups-MacBook-Air.local>
2023-06-01 18:54:34 +0300

    done user list page with update user info

--- src/pages/UserDetailsPage/UserDetailsPage.js
+++ src/pages/UserDetailsPage/UserDetailsPage.js
@@ -1,12 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import { useNavigate } from "react-router-dom";
+import { Button, TextField, Switch, Select, MenuItem } from "@mui/material";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { ROUTE_PATHS } from "../../App.routes";
+
+const useStyles = makeStyles({
+  userListContainer: {
+    display: "flex",
+    alignItems: "flex-end",
+    maxWidth: "800px",
+    margin: "0 auto",
+  },
+  leftSide: {
+    width: "13%",
+    backgroundColor: "#f0f0f0",
+  },
+  rightSide: {
+    flex: 1,
+    marginLeft: "10px",
+    backgroundColor: "#ffffff",
+    padding: "10px",
+  },
+  title: {
+    color: "#777777",
+  },
+  backButton: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "10px",
+  },
+  backButtonIcon: {
+    marginRight: "5px",
+  },
+  form: {
+    display: "grid",
+    gap: "10px",
+    gridTemplateColumns: "1fr 1fr",
+    gridTemplateAreas: `
+      "name email"
+      "phone role"
+      "status status"
+      "button button"
+    `,
+    marginBottom: "10px",
+  },
+  submitButton: {
+    justifySelf: "end",
+    gridArea: "button",
+  },
+});
 
 const UserDetailsPage = () => {
-  // Add user details and update functionality here
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState(true);
+  const [selectedRole, setSelectedRole] = useState("User");
+
+  const handleGoBack = () => {
+    navigate(ROUTE_PATHS.USER_LIST);
+  };
+
+  const handleStatusChange = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleRoleChange = (event) => {
+    setSelectedRole(event.target.value);
+  };
 
   return (
-    <div>
-      <h1>User Details Page</h1>
-      {/* Add user details and update form here */}
+    <div className={classes.userListContainer}>
+      <div className={classes.leftSide} />
+      <div className={classes.rightSide}>
+        <div className={classes.backButton}>
+          <Button
+            style={{
+              color: "#999999",
+            }}
+            onClick={handleGoBack}
+          >
+            <IoIosArrowRoundBack
+              size={"25"}
+              className={classes.backButtonIcon}
+            />
+            Back to Users
+          </Button>
+        </div>
+        <h1 className={classes.title}>Update User</h1>
+        <form className={classes.form}>
+          <TextField label="Name" style={{ gridArea: "name" }} />
+          <TextField label="Email" style={{ gridArea: "email" }} />
+          <TextField label="Phone" style={{ gridArea: "phone" }} />
+          <Select
+            value={selectedRole}
+            onChange={handleRoleChange}
+            style={{ gridArea: "role" }}
+          >
+            <MenuItem value="Admin">Admin</MenuItem>
+            <MenuItem value="User">User</MenuItem>
+          </Select>
+          <div style={{ gridArea: "status" }}>
+            <span>Status: </span>
+            <Switch
+              checked={isActive}
+              onChange={handleStatusChange}
+              color="primary"
+            />
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className={classes.submitButton}
+          >
+            Update
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/UserListPage/UserListPage.js
+++ src/pages/UserListPage/UserListPage.js
@@ -1,6 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import { RiDeleteBinLine, RiArrowRightSLine } from "react-icons/ri";
+import { ROUTE_PATHS } from "../../App.routes";
 
 const useStyles = makeStyles({
   userListContainer: {
@@ -11,34 +23,154 @@ const useStyles = makeStyles({
     width: "13%",
     backgroundColor: "#f0f0f0",
   },
+  title: {
+    color: "#777777",
+  },
   rightSide: {
     flex: 1,
     marginLeft: "10px",
     backgroundColor: "#ffffff",
     padding: "10px",
   },
+  activeStatus: {
+    display: "flex",
+    alignItems: "center",
+  },
+  activeCircle: {
+    width: "10px",
+    height: "10px",
+    borderRadius: "50%",
+    marginRight: "5px",
+  },
+  active: {
+    backgroundColor: "green",
+  },
+  inactive: {
+    backgroundColor: "red",
+  },
+  searchContainer: {
+    marginBottom: "10px",
+    "& .MuiTextField-root": {
+      width: "500px",
+    },
+  },
+  notFoundMessage: {
+    textAlign: "center",
+    marginTop: "20px",
+    fontStyle: "italic",
+  },
 });
 
 const UserListPage = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
 
   const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Smith" },
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john@example.com",
+      phone: "1234567890",
+      role: "Admin",
+      active: true,
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane@example.com",
+      phone: "9876543210",
+      role: "User",
+      active: false,
+    },
   ];
+
+  const deleteUser = (userId) => {
+    console.log(`Deleting user with ID: ${userId}`);
+  };
+
+  const handleUserClick = (userId) => {
+    navigate(ROUTE_PATHS.USER_DETAILS.replace(":id", userId));
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className={classes.userListContainer}>
       <div className={classes.leftSide} />
       <div className={classes.rightSide}>
-        <h1>User List</h1>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              <Link to={`/user/${user.id}`}>{user.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <h1 className={classes.title}>Users</h1>
+        <div className={classes.searchContainer}>
+          <TextField
+            label="Search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            variant="outlined"
+          />
+        </div>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Phone</TableCell>
+                <TableCell>Role</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Action</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredUsers.length === 0 ? (
+                <TableRow>
+                  <TableCell colSpan={7} className={classes.notFoundMessage}>
+                    No users found.
+                  </TableCell>
+                </TableRow>
+              ) : (
+                filteredUsers.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>{user.name}</TableCell>
+                    <TableCell>{user.email}</TableCell>
+                    <TableCell>{user.phone}</TableCell>
+                    <TableCell>{user.role}</TableCell>
+                    <TableCell>
+                      <div className={classes.activeStatus}>
+                        <div
+                          className={`${classes.activeCircle} ${
+                            user.active ? classes.active : classes.inactive
+                          }`}
+                        />
+                        {user.active ? "Active" : "Inactive"}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <Button onClick={() => deleteUser(user.id)}>
+                        <RiDeleteBinLine size={18} />
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        onClick={() => handleUserClick(user.id)}
+                        component={Link}
+                        to={ROUTE_PATHS.USER_DETAILS.replace(":id", user.id)}
+                      >
+                        <RiArrowRightSLine size={25} />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
